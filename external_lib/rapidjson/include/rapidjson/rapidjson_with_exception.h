f6055ec52c0ab11aaff1f113b6ce24676792d9bc
Felix Gündling <felix.guendling@gmail.com>
2013-11-17 14:01:04 +0100

    update packages, add 'rapidjson_with_exception.h'

---
+++ external_lib/rapidjson/include/rapidjson/rapidjson_with_exception.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2013, makielski.net
+// Licensed under the MIT license
+// https://raw.github.com/makielski/botscript/master/COPYING
+
+#ifndef RAPIDJSON_WITH_EXCEPTION_H_
+#define RAPIDJSON_WITH_EXCEPTION_H_
+
+#include <stdexcept>
+
+namespace botscript_server {
+
+class rapidjson_exception : public std::runtime_error {
+ public:
+  rapidjson_exception() : std::runtime_error("json schema invalid") {
+  }
+};
+
+}  // namespace botscript_server
+
+#define RAPIDJSON_ASSERT(x) {\
+  if(x) { \
+  } else { throw botscript_server::rapidjson_exception(); } \
+}
+
+#include <rapidjson/document.h>
+
+#endif  // RAPIDJSON_WITH_EXCEPTION_H_
